94c77043367cac81e327744b7f1085f63ef374a8
Dexter Ma <dexterma@MacBook-Air.local>
2021-07-15 21:46:46 -0700

    Refactored ejs code.

--- public/javascript/codeEditor.js
+++ public/javascript/codeEditor.js
@@ -62,9 +62,9 @@ function copyEmbeddedCode(id) {
     // Select and copy text into clipboard
     const text = $(`#${id}`).select();
     document.execCommand('copy');
-    // Hide popup and show successfully copy toast. This shows all toasts.
+    // Hide popup and show successfully copy toast.
     $('#embedCodeModal').modal('hide');
-    $('.toast').toast({delay:1000}).toast('show');
+    $('#copiedToast').toast({delay:1000}).toast('show');
 }
 
 function createLinktoCode() {
