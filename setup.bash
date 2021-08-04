f32e577e06399c718e1c5f39e10b2ad1cd55be0a
Taka Kodani <tklkdn@gmail.com>
2021-08-03 19:32:35 -0700

    setupEnv fix

--- setup.bash
+++ setup.bash
@@ -5,14 +5,14 @@ wasmtime_version="v0.28.0"
 
 if [ ! -d "wasi-sdk-$wasi_sdk_version" ] &> /dev/null; then
     echo "Installing wasi-sdk"
-    wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${wasi_sdk_version::-2}/wasi-sdk-$wasi_sdk_version-linux.tar.gz
+    wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${wasi_sdk_version::-2}/wasi-sdk-$wasi_sdk_version-linux.tar.gz > /dev/null 2>&1
     tar -xf wasi-sdk-$wasi_sdk_version-linux.tar.gz
     rm wasi-sdk-$wasi_sdk_version-linux.tar.gz
 fi
 
-if [ -d "wasmtime-$wasmtime_version-x86_64-linux" ] &> /dev/null; then
+if [ ! -d "wasmtime-$wasmtime_version-x86_64-linux" ] &> /dev/null; then
   echo "Installing wasmtime"
-    wget https://github.com/bytecodealliance/wasmtime/releases/download/$wasmtime_version/wasmtime-$wasmtime_version-x86_64-linux.tar.xz
+    wget https://github.com/bytecodealliance/wasmtime/releases/download/$wasmtime_version/wasmtime-$wasmtime_version-x86_64-linux.tar.xz > /dev/null 2>&1
     tar -xf wasmtime-$wasmtime_version-x86_64-linux.tar.xz
     rm wasmtime-$wasmtime_version-x86_64-linux.tar.xz
 fi
